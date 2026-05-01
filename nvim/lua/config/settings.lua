1810cca2c82e831664b49f3a16ebdf25ad47e000
Daniel <96448621+danisot@users.noreply.github.com>
2026-05-01 12:27:31 +0200

    feat(neovim): add custom editor settings
    Create file with editor options and keymaps

---
+++ nvim/lua/config/settings.lua
@@ -0,0 +1,11 @@
+-- Options
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.scrolloff = 10
+vim.o.undofile = true
+
+-- Keymaps
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
